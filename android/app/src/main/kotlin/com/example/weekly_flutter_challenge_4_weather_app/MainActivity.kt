24723e750e5b507633cfe59cca64c553fae553ed
JKPK <jacek.kawiak@gmail.com>
2019-09-29 19:06:10 +0200

    Initial source code.

---
+++ android/app/src/main/kotlin/com/example/weekly_flutter_challenge_4_weather_app/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.weekly_flutter_challenge_4_weather_app
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
