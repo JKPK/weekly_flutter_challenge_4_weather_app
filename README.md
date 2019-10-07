c870330f77fa4747686e13ce198f96380dd5dcf9
JKPK <jacek.kawiak@gmail.com>
2019-10-07 21:52:44 +0200

    Added action to notification button. Created Dialog.

--- README.md
+++ README.md
@@ -5,5 +5,12 @@ This challenge is about recreating Ashok Rajan's Weather App Design. The goal is
 
 https://www.uplabs.com/posts/weather-app-244f14ad-518c-4244-bc75-05015676171a
 
+What I have learned during this challenge (in no particular order):
+
+1. Material widget.
+2. Showing custom Dialog.
+3. Spacer widget.
+4. Radio widget.
+
 ## Original design
 [![Original design](https://github.com/JKPK/weekly_flutter_challenge_4_weather_app/blob/master/original_design.gif?raw=true)](https://www.uplabs.com/posts/weather-app-244f14ad-518c-4244-bc75-05015676171a)
--- lib/widgets/notification_button.dart
+++ lib/widgets/notification_button.dart
@@ -1,35 +1,47 @@
 import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../styleguide.dart';
+import '../providers/reminder_provider.dart';
+import '../widgets/reminder_dialog.dart';
+
 
 class NotificationButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List<String> reminders =
+        Provider.of<ReminderProvider>(context, listen: false).reminders;
+
     return Positioned(
       left: MediaQuery.of(context).size.width * .1,
       top: MediaQuery.of(context).size.height * .43,
-      child: Container(
-        width: MediaQuery.of(context).size.width * .15,
-        height: MediaQuery.of(context).size.width * .15,
-        decoration: BoxDecoration(
-          color: parisColor,
-          shape: BoxShape.circle,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.2),
-              blurRadius: 6,
-              offset: Offset(0, 6),
+      child: Material(
+        shape: CircleBorder(),
+        clipBehavior: Clip.antiAlias,
+        color: parisColor,
+        elevation: 7,
+        child: InkWell(
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return ReminderDialog(reminders);
+              },
+            );
+          },
+          child: Container(
+            width: MediaQuery.of(context).size.width * .15,
+            height: MediaQuery.of(context).size.width * .15,
+            child: Transform.rotate(
+              angle: -math.pi * .15,
+              child: Icon(
+                Icons.notifications_none,
+                color: Colors.white,
+                size: 35,
+              ),
             ),
-          ],
-        ),
-        child: Transform.rotate(
-          angle: -math.pi * .15,
-          child: Icon(
-            Icons.notifications_none,
-            color: Colors.white,
-            size: 35,
           ),
         ),
       ),
